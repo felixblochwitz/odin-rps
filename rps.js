ad0382ba8bb0304bf60a5a76d3e8af01391502a4
felixblochwitz <felix.blochwitz@gmail.com>
2023-03-26 21:40:13 +0200

    Add functionality to show round choices and result
    This adds functions to show images of the choices of the players
    and the result of win or lose as a text

--- rps.js
+++ rps.js
@@ -1,26 +1,56 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissors"];
   let randomNumber = Math.floor(Math.random() * choices.length);
-  return choices[randomNumber]
+  return choices[randomNumber];
+}
+
+function showChoices(humanChoice, computerChoice) {
+  const imageMap = {
+    rock: "images/rock_ohne.png",
+    paper: "images/paper_ohne.png",
+    scissors: "images/scissors_ohne.png",
+  };
+  const humanChoiceImg = document.querySelector("#human-choice-img");
+  const computerChoiceImg = document.querySelector("#computer-choice-img");
+
+  humanChoiceImg.src = imageMap[humanChoice];
+  computerChoiceImg.src = imageMap[computerChoice];
+}
+
+function showRoundResultText(humanChoice, computerChoice, roundResult) {
+  const roundResultElement = document.querySelector("#round-result-text");
+  let resultString = "";
+  if (roundResult === "WIN" || roundResult === "LOSE") {
+    resultString = `You chose ${humanChoice}, the computer chose ${computerChoice}.
+      <br>You ${roundResult}!`;
+  } else {
+    resultString = `You both made the same choice, leading to the saddest result in sports.
+      <br>It's a ${roundResult}!`;
+  }
+  roundResultElement.innerHTML = resultString;
 }
 
 function compete(humanChoice, computerChoice) {
+  showChoices(humanChoice, computerChoice);
   if (humanChoice === computerChoice) {
-    console.log(humanChoice);
-    console.log(computerChoice);
-    console.log("Draw");
+    const roundResult = "DRAW";
+    showRoundResultText(humanChoice, computerChoice, roundResult);
+    updateScore(roundResult);
   } else if (
     (humanChoice === "rock" && computerChoice === "scissors") ||
     (humanChoice === "paper" && computerChoice === "rock") ||
     (humanChoice === "scissors" && computerChoice === "paper")
   ) {
-    console.log(humanChoice);
-    console.log(computerChoice);
-    console.log("Win!");
+    const roundResult = "WIN";
+    showRoundResultText(humanChoice, computerChoice, roundResult);
+    updateScore(roundResult);
   } else {
-    console.log(humanChoice);
-    console.log(computerChoice);
-    console.log("Lose!");
+    const roundResult = "LOSE";
+    showRoundResultText(humanChoice, computerChoice, roundResult);
+    updateScore(roundResult);
   }
 }
 
