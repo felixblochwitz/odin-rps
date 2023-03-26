34f934247001438694177a4073fb009c612cfe42
felixblochwitz <felix.blochwitz@gmail.com>
2023-03-26 23:40:31 +0200

    Implement first to 5 game logic
    Creates an observer that looks for either player or computer
    score to get to 5.
    As soon as that happens, an alert with game over pops up
    and the score and choice pictures are reset.

--- rps.js
+++ rps.js
@@ -31,13 +31,13 @@ function showRoundResultText(humanChoice, computerChoice, roundResult) {
 }
 
 function updateScore(roundResult) {
+  const humanScoreSpan = document.querySelector("#player-score");
+  const computerScoreSpan = document.querySelector("#computer-score");
+  let humanScore = parseInt(humanScoreSpan.textContent);
+  let computerScore = parseInt(computerScoreSpan.textContent);
   if (roundResult === "WIN") {
-    const humanScoreSpan = document.querySelector("#player-score");
-    let humanScore = parseInt(humanScoreSpan.textContent);
     humanScoreSpan.textContent = humanScore += 1;
   } else if (roundResult === "LOSE") {
-    const computerScoreSpan = document.querySelector("#computer-score");
-    let computerScore = parseInt(computerScoreSpan.textContent)
     computerScoreSpan.textContent = computerScore += 1;
   }
 }
@@ -63,6 +63,36 @@ function compete(humanChoice, computerChoice) {
   }
 }
 
+function resetGame() {
+  const choiceImages = document.querySelectorAll(".choice-img");
+  const competitorScores = document.querySelectorAll(".competitor-scores");
+  const resultText = document.querySelector("#round-result-text");
+  alert("Game Over");
+  choiceImages.forEach(img => img.src = "");
+  competitorScores.forEach(score => score.textContent = "0");
+  resultText.textContent = "Who will win?"
+}
+
+const playerScore = document.querySelector("#player-score");
+const computerScore = document.querySelector("#computer-score");
+
+const observer = new MutationObserver(function (mutations) {
+  console.log(mutations);
+  mutations.forEach(function (mutation) {
+    if (
+      mutation.type === "childList" &&
+      [playerScore, computerScore].includes(mutation.target) &&
+      mutation.target.textContent === "5"
+    ) {
+      setTimeout(resetGame, 10);
+    }
+  });
+});
+
+const config = { childList: true };
+observer.observe(playerScore, config);
+observer.observe(computerScore, config);
+
 const rockButton = document.querySelector("#play-button-rock");
 rockButton.addEventListener("click", function () {
   compete("rock", getComputerChoice());
@@ -77,3 +107,5 @@ const scissorsButton = document.querySelector("#play-button-scissors");
 scissorsButton.addEventListener("click", function () {
   compete("scissors", getComputerChoice());
 });
+
+let lilTest = [1, 2];
